8016b6f7af9744c1c1aee87f73302cad1abbfac1
shenoragn <shenoranglin95@gmail.com>
2021-10-24 23:52:08 -0500

    JavaScript or nah

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
 /* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', () => {
+    function eSubscribe(e) {
+        e.preventDefault();
+        var email = document.getElementById(email);
+        alert(email);
+        if (email != null){
+            var message = ("Thank you! Your email address " + email.value + " has been added to our mailing list");
+            alert(message);
+        }
+    }
+    document.getElementById("btn").addEventListener("submit", eSubscribe);
+})
